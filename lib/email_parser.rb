79769dfb0e81ee5b0689687d2fc7114ecc46a3ef
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-21 21:55:27 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,4 +3,8 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
+  attr_accessor :email
+  def initialize(email)
+    @email = email
+  end
 end
